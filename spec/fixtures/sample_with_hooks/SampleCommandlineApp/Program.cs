0a16965727cfac077ef7573015c961f19a8a696b
David Laing <david@davidlaing.com>
2013-09-11 18:12:57 +0100

    Add sample with hooks

---
+++ spec/fixtures/sample_with_hooks/SampleCommandlineApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SampleCommandlineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("{0}\tDoing mock work", DateTime.UtcNow.ToString("u"));
+                System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
